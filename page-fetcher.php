9e65d1f297cf6109a1318d7b9c810ca7ebd2add5
linqiuyu <qiuyu_lin@fzzixun.com>
2021-03-22 09:26:17 +0800

    安装依赖

---
+++ page-fetcher.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Page Fetcher
+ * Plugin URI: https://github.com/linqiuyu/page-fetcher
+ * Description: 页面采集工具
+ * Version: 1.0.0
+ * Requires PHP: 7.4
+ * Author: linqiuyu
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: page-fetcher
+ * Domain Path: languages
+ */
